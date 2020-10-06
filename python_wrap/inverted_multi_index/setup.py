f40b188382ef3f98e0ae3d9a7035f792d2e0fb99
dmitriy.borisov <dmitriy.borisov@pm.bet>
2020-10-06 12:24:52 +0400

    extra_compile_args

--- python_wrap/inverted_multi_index/setup.py
+++ python_wrap/inverted_multi_index/setup.py
@@ -7,21 +7,23 @@ import os
 
 extensions = [
     Extension("py_inverted_multi_index", ["py_inverted_multi_index.pyx"],
-              include_dirs=[r"c:\SourcePython\inverted_multi_index\src",
-                            "c:\SourcePython\inverted_multi_index\OpenBLAS-v0.2.19-Win64-int32\include",
+              include_dirs=[r"c:\SourceOpen\inverted_multi_indexMy\src",
+                            "c:\SourceOpen\inverted_multi_indexMy\OpenBLAS-v0.2.19-Win64-int32\include",
                             get_include()],
               libraries=[r'libopenblas.dll'],
-              library_dirs=[r'c:\SourcePython\inverted_multi_index\OpenBLAS-v0.2.19-Win64-int32\lib']
+              library_dirs=[r'c:\SourceOpen\inverted_multi_indexMy\OpenBLAS-v0.2.19-Win64-int32\lib'],
+              extra_compile_args=["-Zi", "/Od"],
+              extra_link_args=["-debug"],
               ),
     Extension("py_multi_index_util", ["py_multi_index_util.pyx"],
-              include_dirs=[r"c:\SourcePython\inverted_multi_index\src",
-                            get_include()]
+              include_dirs=[r"c:\SourceOpen\inverted_multi_indexMy\src",
+                            get_include()],
+              extra_compile_args=["-Zi", "/Od"],
+              extra_link_args=["-debug"],
               )
 ]
 
 setup(
     name="build_inverted_multi_index",
     ext_modules=cythonize(extensions),
-    extra_compile_args=["-Zi", "/Od"],
-    extra_link_args=["-debug"],
 )
